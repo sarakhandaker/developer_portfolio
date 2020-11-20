bbfd981e891cf3500c6e74e16772356278cd7cc6
sarakhandaker <kttsara@gmail.com>
2020-11-20 10:34:14 -0800

    updated about me

--- src/components/About.js
+++ src/components/About.js
@@ -14,12 +14,10 @@ class About extends Component {
                             <img alt="techlogos" src={techlogo} className="techlogo" width="470"/>
                             <br /><br />
                             I'm Sara, a full stack software engineer based in Seattle, WA.<br /><br />
-                             I have a background in Civil/ Geotechnical Engineering and I enjoy helping people interact with the city/ spaces they live in.<br /><br />
+                             I have a background in Civil Engineering and I enjoy helping people interact with technology to make their lives easier.<br /><br />
                              I have experience on large engineering projects focused on clients' needs to deliver quality products. My range of international work experience in Germany, Canada, Tunisia, and the USA offers me a unique and global perspective.<br /><br />
-                             My goal is to use my strong technical expertise to innovate software solutions for the smart urban environments of tomorrow.
-                             I am passionate about sustainable, environmental design with a keen insterest in public transit.<br /><br />
+                             My goal is to use my strong technical expertise and creative problem solving skills to innovate software solutions.<br /><br />
                              I recently graduated from Flatiron School's Software Engineering Program.<br /><br />
-                             <a className="btn btn-secondary" href="https://gist.github.com/sarakhandaker/23951838952ece007e6d94b9d21d919e">See My Resume</a>
                         </div>
                         <div className="col-lg-4" style={{ "color": "black", "fontSize": "20px" }}>
                             <div className="list-type4">
@@ -27,6 +25,7 @@ class About extends Component {
                                     <li>JavaScript</li>
                                     <li>Ruby</li>
                                     <li>C#</li>
+                                    <li>Python</li>
                                     <li>SQL</li>
                                     <li>HTML &amp; CSS</li>
                                     <li>Bootstrap</li>
